3c47bfab8e4a14a8100b359740564d91cf71b361
VorakornK <vorakorn.ko@ku.th>
2023-02-09 14:23:04 +0700

    update HTTP method

--- scp/src/main.cpp
+++ scp/src/main.cpp
@@ -3,6 +3,10 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
+//ID 1, 2
+//Floor 1
+
+
 const char *ssid = "Qwerty";
 const char *password = "12345678";
 
@@ -28,4 +32,41 @@ void Connect_Wifi() {
   }
   Serial.print("OK! IP=");
   Serial.println(WiFi.localIP());
+}
+
+void Send_Park(int id, int floor, bool Status) {
+  //Status = true มีรถจอด
+  //Status = false ไม่มีรถจอด
+  HTTPClient http;
+  DynamicJsonDocument doc(512);
+  doc["id"] = id;
+  doc["floor"] = floor;
+  doc["status"] = Status;
+  http.begin(Send_Park_url);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode >= 200 && httpResponseCode < 300) {
+    Serial.println("Done!!");
+    }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
+void Send_Check(int floor, bool IN) {
+  //IN = true รถเข้า
+  //IN = false รถออก
+  HTTPClient http;
+  DynamicJsonDocument doc(512);
+  doc["floor"] = floor;
+  doc["running_count"] = IN ? 1 : -1;
+  http.begin(Send_Check_url);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode >= 200 && httpResponseCode < 300) {
+    Serial.println("Done!!");
+    }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
 }

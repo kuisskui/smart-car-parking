e23674c526c123b6cb9589266df184d86942f3c8
VorakornK <vorakorn.ko@ku.th>
2023-02-09 13:46:47 +0700

    update base code

--- scp/src/main.cpp
+++ scp/src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <Bounce2.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char *ssid = "Qwerty";
+const char *password = "12345678";
+
+const String Send_Park_url = "";
+const String Send_Check_url = "";
+
+void Connect_Wifi();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Connect_Wifi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
+
+void Connect_Wifi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.print("OK! IP=");
+  Serial.println(WiFi.localIP());
 }

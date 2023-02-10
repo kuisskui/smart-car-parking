0c12a20b3d70fcb1d13dae3604482da25b9c4de3
GolfGrab <surapus01@gmail.com>
2023-02-10 13:23:30 +0700

    fix css bug

--- frontend/src/components/ParkingStatus.tsx
+++ frontend/src/components/ParkingStatus.tsx
@@ -8,13 +8,17 @@ const ParkingStatus: React.FC<ParkingStatusProps> = ({
   carRunning,
 }) => {
   return (
-    <footer className="footer p-5 bg-neutral text-neutral-content">
-      <div className="flex w-full flex-row justify-evenly p-5 ">
-        <h1 className="text-5xl"> ที่จอดรถว่าง <span className="text-error">{carParkingRemain}</span>  คัน </h1>
-        <h1 className="text-5xl "> รถขับวนอยู่ <span className="text-error">{carRunning}</span> คัน </h1>
+    <footer className="flex h-[128px] items-center justify-center bg-neutral  text-neutral-content">
+      <div className="flex w-full flex-row justify-evenly  ">
+        <h1 className="text-5xl">
+          ที่จอดรถว่าง
+          <span className="px-4 text-error">{carParkingRemain}</span>คัน
+        </h1>
+        <h1 className="text-5xl ">
+          รถขับวนอยู่<span className="px-4 text-error">{carRunning}</span>คัน
+        </h1>
       </div>
     </footer>
-
   );
 };
 

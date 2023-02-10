448d14b086046193325926f764a06e1faf4f41eb
VorakornK <vorakorn.ko@ku.th>
2023-02-10 10:13:35 +0700

    uodate check park

--- scp/src/main.cpp
+++ scp/src/main.cpp
@@ -3,19 +3,32 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-#define LDR_IN     13;
-#define LDR_OUT    2;
-#define SENSOR_1   32;
-#define SENSOR_2   39;
-#define LED_1      34;
-#define LED_2      36;
+#define LDR_IN     13
+#define LDR_OUT    2
+#define SENSOR_1   32
+#define SENSOR_2   39
+#define LED_1      34
+#define LED_2      36
 
+const int dark = 2000;
 
 //ID 1, 2
 //Floor 1
 
+bool currentStatus[2] = {false, false};
+int changeStatus[2] = {0, 0};
+int AllSensor[2] = {SENSOR_1, SENSOR_2};
+
+int currentFloor = 1;
+
 Bounce debouncer1 = Bounce(); 
-Bounce debouncer2 = Bounce(); 
+Bounce debouncer2 = Bounce();
+
+TaskHandle_t Checking = NULL;
+TaskHandle_t Sending = NULL;
+
+int globalID;
+bool globalState;
 
 const char *ssid = "Qwerty";
 const char *password = "12345678";
@@ -23,8 +36,13 @@ const char *password = "12345678";
 const String Send_Park_url = "";
 const String Send_Check_url = "";
 
+int isObstacle = HIGH;
+
+
 void Send_Park(int id, int floor, bool Status);
 void Send_Check(int floor, bool IN);
+void Send(void *param);
+void Check(void *param);
 
 
 void Connect_Wifi();
@@ -32,9 +50,51 @@ void Connect_Wifi();
 void setup() {
   Serial.begin(115200);
   Connect_Wifi();
+  xTaskCreatePinnedToCore(Check, "Checking", 10000, NULL, 1, &Checking, 0);
 }
 
 void loop() {
+}
+
+void Check(void *param) {
+  while (true) {
+    Check_Park(1);
+    Check_Park(2);
+    vTaskDelay(1000);
+  }
+}
+
+void Check_Park(int id) {
+  //id = 1, 2
+  id--;
+  int sensor = AllSensor[id];
+  bool status = analogRead(sensor) < dark;
+  //status mean isDark
+  if (status && !currentStatus[id] && changeStatus[id] < 10) {
+    changeStatus[id]++;
+  }
+  else if (!status && !currentStatus[id]) {
+    changeStatus[id] = 0;
+  }
+  else if (status && currentStatus[id]) {
+    changeStatus[id] = 10;
+  }
+  else if (!status && currentStatus[id] && changeStatus[id] > 0) {
+    changeStatus[id]--;
+  }
+  //check if need to send http
+  if ((!currentStatus[id] && changeStatus[id] == 10) || (currentStatus[id] && changeStatus[id] == 0)) {
+    //switch state
+    currentStatus[id] = !currentStatus[id];
+    globalID = id + 1;
+    globalState = (!currentStatus[id] && changeStatus[id] == 10);
+    xTaskCreatePinnedToCore(Send, "Sending", 10000, NULL, 1, &Sending, 1);
+  }
+}
+
+void Send(void *param) {
+  Send_Park(globalID, currentFloor, globalState);
+  Send_Check(currentFloor, !globalState);
 }
 
 void Connect_Wifi() {
@@ -48,13 +108,13 @@ void Connect_Wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void Send_Park(int id, int floor, bool Status) {
+void Send_Park(int id, int currentFloor, bool Status) {
   //Status = true มีรถจอด
   //Status = false ไม่มีรถจอด
   HTTPClient http;
   DynamicJsonDocument doc(512);
   doc["id"] = id;
-  doc["floor"] = floor;
+  doc["floor"] = currentFloor;
   doc["status"] = Status;
   http.begin(Send_Park_url);
   int httpResponseCode = http.GET();
